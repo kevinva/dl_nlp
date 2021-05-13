ee552ab9db4a33d51cf446835636bc42a48ffc3e
hezhi <hezhi@yy.com>
2021-05-13 18:04:54 +0800

    Test

--- ch01/ex.py
+++ ch01/ex.py
@@ -3,7 +3,14 @@ sys.path.append('..')
 
 from dataset import spiral
 import matplotlib.pyplot  as plt
+import numpy as np
 
-x, t = spiral.load_data()
-print('x', x[0:10, :])
-print('t', t[0:10, :])
+# x, t = spiral.load_data()
+# print('x', x[0:10, :])
+# print('t', t[0:10, :])
+y = np.random.randn(10, 3)
+t = y.argmax(axis=1)
+print(y)
+# print(y.argmax(axis=1))
+print(t)
+print(y[np.arange(10), t])
--- common/layers.py
+++ common/layers.py
@@ -7,22 +7,25 @@ from common.functions import softmax, cross_entropy_error
 
 class Sigmoid:
     def __init__(self):
-        self.params = []
+        self.params, self.grads = [], []
         self.out = None
     
     def forward(self, x):
         out = 1 / (1 + np.exp(-x))
         self.out = out
+
+        # print('sigmoid forward: x {}, out: {}'.format(x.shape, out.shape))
         return out
 
     def backward(self, dout):
         dx = dout * (1.0 - self.out) * self.out
+        # print('sigmoid backward: dout: {}, dx: {}'.format(dout.shape, dx.shape))
         return dx
 
 class SigmoidWithLoss:
     def __init__(self):
         self.params, self.grads = [], []
-
+    # hoho_todo
 
 class Softmax:
     def __init__(self):
@@ -50,10 +53,13 @@ class SoftmaxWithLoss:
         self.t = t
         self.y = softmax(x)
 
-        if self.t.size == self.y.size:  # hoho_unknown
+        if self.t.size == self.y.size:
             self.t = self.t.argmax(axis=1)
         
         loss = cross_entropy_error(self.y, self.t)
+
+        # print('SoftmaxWithLoss forward: x {}, t {}, loss {}'.format(x.shape, t.shape, loss))
+
         return loss
 
     def backward(self, dout=1):
@@ -63,6 +69,8 @@ class SoftmaxWithLoss:
         dx[np.arange(batch_size), self.t] -= 1
         dx *= dout
         dx = dx / batch_size
+
+        # print('SoftmaxWithLoss backward: dx {}'.format(dx.shape))
 
         return dx
 
@@ -76,6 +84,8 @@ class Affine:
         W, b = self.params
         out = np.dot(x, W) + b
         self.x = x
+
+        # print('Affine forward: W {}, b {}, x {}, out {}'.format(W.shape, b.shape, x.shape, out.shape))
         return out
 
     def backward(self, dout):
@@ -86,6 +96,8 @@ class Affine:
 
         self.grads[0][...] = dW
         self.grads[1][...] = db
+
+        # print('Affine backward: W {}, dW {}, b {}, db {}, dx {}'.format(W.shape, dW.shape, b.shape, db.shape, dx.shape))
 
         return dx
 
