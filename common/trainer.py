1ba128765d41eb141156d52d7c76b2a06d91a08f
hezhi <hezhi@yy.com>
2021-05-19 18:33:51 +0800

    word2vec 基础

--- common/trainer.py
+++ common/trainer.py
@@ -45,7 +45,7 @@ class Trainer:
                 if (eval_interval is not None) and (iters % eval_interval) == 0:
                     avg_loss = total_loss / loss_count
                     elapsed_time = time.time() - start_time
-                    print('| epoch %d | iter %d / %d | loss %.2f' % (self.current_epoch + 1, iters + 1, max_iters, elapsed_time, avg_loss))
+                    print('| epoch %d | iter %d / %d | time %d[s] | loss %.2f' % (self.current_epoch + 1, iters + 1, max_iters, elapsed_time, avg_loss))
                     self.loss_list.append(float(avg_loss))
                     total_loss, loss_count = 0, 0
             self.current_epoch += 1
