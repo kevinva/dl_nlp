dba40040eea2b07b9ad21694ba4667114a5d6034
hezhi <hezhi@yy.com>
2021-05-25 18:41:11 +0800

    Test

--- common/layers.py
+++ common/layers.py
@@ -115,10 +115,52 @@ class MatMul:
 
     def backward(self, dout):   # dout为上游传过来的梯度
         W, = self.params
-        dx = np.dot(dout, W.T)
-        dW = np.dot(self.x.T, dout)
+        dx = np.dot(dout, W.T)   # dx表示损失函数对x求偏导
+        dW = np.dot(self.x.T, dout)  # dW表示损失函数对W求偏导
         self.grads[0][...] = dW
         return dx
+
+
+class Embedding:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.idx = None
+    
+    def forward(self, idx):
+        W, = self.params
+        self.idx = idx
+        out = W[idx]
+        return out
+    
+    def backward(self, dout):
+        dW, = self.grads
+        dW[...] = 0
+
+        for i, word_id in enumerate(self.idx):
+            dW[word_id] += dout[i]
+
+
+class EmbeddingDot:
+    def __init__(self, W):
+        self.embed = Embedding(W)
+        self.params = self.embed.params
+        self.grads = self.embed.grads
+        self.cache = None
+
+    def forward(self, h, idx):
+        target_W = self.embed.forward(idx)
+        out = np.sum(target_W * h, axis=1)
+        self.cache = (h, target_W)
+        return out
+
+    def backward(self, dout):
+        h, target_W = self.cache
+        dout = dout.reshape(dout.shape[0], 1)
+        dtarget_W = dout * h
+        self.embed.backward(dtarget_W)
+        dh = dout * target_W
+        return dh
 
 
 if __name__ == '__main__':
