cd9d623bf3c380c4fc17b6f0b09cd555cf209d2b
hezhi <hezhi@yy.com>
2021-06-04 18:56:40 +0800

    LSTM

--- common/util.py
+++ common/util.py
@@ -114,7 +114,20 @@ def convert_one_hot(corpus, vocab_size):
             for idx_1, word_id in enumerate(word_ids):
                 one_hot[idx_0, idx_1, word_id] = 1
 
-    return one_hot 
+    return one_hot
+
+
+def clip_grads(grads, max_norm):
+    total_norm = 0
+    for grad in grads:
+        total_norm += np.sum(grad ** 2)
+    total_norm = np.sqrt(total_norm)
+
+    rate = max_norm / (total_norm + 1e-6)
+
+    if rate < 1:
+        for grad in grads:
+            grad *= rate
 
 
 if __name__ == '__main__':
